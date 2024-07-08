9f9cae2023519d9f5e79093bb6ee48e31496145b
Matteo C <hello@micorksen.eu>
2024-07-08 13:28:27 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,6 +8,9 @@ git clone https://github.com/open-ent-ng/theme-open-ent
 git clone https://github.com/acverne/theme-open-ent /tmp/verne-theme-open-ent
 echo "Cloned the two repositories."
 
-cp -TR /tmp/verne-theme-open-ent/{assets,overrides,template} ./theme-open-ent/
-rm -R /tmp/verne-theme-open-ent
+for directory in assets overrides template; do
+  rm -rf ./theme-open-ent/$directory/
+  cp -R /tmp/verne-theme-open-ent/$directory/ ./theme-open-ent/
+done
+
 echo "Done."
