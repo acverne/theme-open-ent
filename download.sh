9a6300fb166839f09d83d7d047b1cfc2da7590b1
Matteo C <hello@micorksen.eu>
2024-07-08 13:35:21 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,7 +8,7 @@ git clone https://github.com/open-ent-ng/theme-open-ent
 git clone https://github.com/acverne/theme-open-ent /tmp/verne-theme-open-ent
 echo "Cloned the two repositories."
 
-for directory in assets overrides template; do
+for directory in assets/i18n assets/img/smileys assets/js overrides template; do
   rm -rf ./theme-open-ent/$directory/
   cp -R /tmp/verne-theme-open-ent/$directory/ ./theme-open-ent/
 done
